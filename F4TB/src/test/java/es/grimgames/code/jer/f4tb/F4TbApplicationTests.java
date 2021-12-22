1af845c66fbd728d16dc491f569232312fab4fca
Juanchinacio <64609838+Juanchinacio@users.noreply.github.com>
2021-12-22 02:35:11 +0100

    Prueba

---
+++ F4TB/src/test/java/es/grimgames/code/jer/f4tb/F4TbApplicationTests.java
@@ -0,0 +1,13 @@
+package es.grimgames.code.jer.f4tb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F4TbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
